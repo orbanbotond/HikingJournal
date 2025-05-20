0987eb9860cc2ebfad094b4757e8af0f292b79a8
Orban Botond <orbanbotond@gmail.com>
2025-05-20 14:02:12 +0300

    Initial Commit

---
+++ HikingJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HikingJournal
+//
+//  Created by Botond Orban, Logic Optimum ltd. 28529878 on 5/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
